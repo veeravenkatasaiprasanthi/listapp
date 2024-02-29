eb4090b0484f8e1b2a4bffeef049a7a246fdad22
veeravenkatasaiprasanthi <104972610+veeravenkatasaiprasanthi@users.noreply.github.com>
2024-02-29 20:36:27 +0530

    Create colors.dart

---
+++ colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color.fromARGB(255, 111, 248, 104);
+const Color tdBlue = Color(0xFF5F52EE);
+
+const Color tdBlack = Color.fromARGB(255, 18, 8, 34);
+const Color tdGrey = Color.fromARGB(255, 252, 70, 219);
+
+const Color tdBGColor = Color.fromARGB(255, 107, 107, 107);
